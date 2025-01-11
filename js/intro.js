701a47053c3b0682600b2b5468cb4c7777f93d51
Key <kpillorsrocks@gmail.com>
2025-01-11 12:55:08 -0500

    Improve general usability and responsiveness

--- js/intro.js
+++ js/intro.js
@@ -2,9 +2,13 @@
 
 let newX = 0, newY = 0, startX = 0, startY = 0
 
-const key = document.getElementById('decoy')
+const key = document.getElementById('decoy');
+const viewportWidth = window.innerWidth;
+const viewportHeight = window.innerHeight;
 
 console.log({key})
+console.log({viewportWidth})
+console.log({viewportHeight})
 
 key.addEventListener('dragstart', mouseDown)
 
@@ -27,8 +31,18 @@ function mouseMove(e){
     key.style.top = startY + 'px'
     key.style.left = startX + 'px'
 
+    console.log({startX, startY})
+
+    if ((Math.round((viewportWidth / 2)) + 20) > startX 
+        && startX > (Math.round((viewportWidth / 2)) - 100)
+        && (Math.round((viewportWidth / 2)) + 100) > startY 
+        && startY > (Math.round((viewportWidth / 2)) - 80)) {
+        window.location.href = "home.html";
+    }
 }
 
 function mouseUp(e){
     document.removeEventListener('mousemove', mouseMove)
 }
+
+//  && startY == (Math.round(viewportHeight / 2) - 80)
